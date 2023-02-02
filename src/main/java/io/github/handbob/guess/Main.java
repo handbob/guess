b106400a4f9b34ffc058731504d20a6d57cafc52
handbob <handbob@icloud.com>
2023-02-02 22:47:30 +0100

    change project

--- src/main/java/io/github/handbob/guess/Main.java
+++ src/main/java/io/github/handbob/guess/Main.java
@@ -1,7 +1,46 @@
 package io.github.handbob.guess;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        try (Scanner scanner = new Scanner(System.in)) {
+            String message = new String();
+            String option = new String();
+            String input = new String();
+            Random random = new Random();
+            Integer number;
+            Integer guess;
+
+            guess = random.nextInt(1, 10);
+            
+            message = "\nInteractive commandline game guess the number!\nChoose one option:\n1. Play\n2. Exit\nOption: ";
+
+            System.out.printf("%s", message);
+            option = scanner.nextLine();
+            switch (option) {
+                case "1":
+                    message = "The guessing number is from range 1 to 10\nEnter number: ";
+                    System.out.printf("%s", message);
+                    do {
+                        input = scanner.nextLine();
+                        number = Integer.parseInt(input);
+                        if (number < 1 && number > 10 || number != guess) {
+                            message = "Try again\nEnter number from range 1 to 10: ";
+                            System.out.printf("%s", message);
+                        }
+                        else {
+                            message = "\nYou have found the guessing number";
+                            System.out.printf("%s: %d = %d\n", message, guess, number);
+                            break;
+                        }
+                    } while (true);
+                case "2":
+                    message = "Thank you for playing!";
+                    System.out.printf("%s\n", message);
+                    break;
+            }
+        }
     }
 }
